9f3dfd53a14bce74d04dea803dae760b3e04fa42
Jonathan Picazo <jonathanpicazo@outlook.com>
2024-01-21 04:53:32 -0800

    chore: swapped images to license-free versions

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -12,10 +12,14 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ service }) => {
       <h3 className="mb-3 text-lg text-primary md:text-xl">{title}</h3>
       <div className="relative h-[300px]">
         <Image
+          priority
           src={image.src}
           className="h-auto w-auto rounded-lg object-cover object-top"
           alt={title}
           fill
+          sizes="(max-width: 600px) 100vw,
+                 (max-width: 1200px) 50vw,
+                  1200px"
         />
       </div>
       <p className="mt-2 md:mt-4">{description}</p>
--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -9,13 +9,16 @@ const Hero: React.FC = () => {
   const pathname = usePathname();
   return (
     pathname === '/' && (
-      <div className="relative mx-auto -mt-3 mb-3 h-[300px] max-w-screen-2xl md:-mt-5">
+      <div className="relative mx-auto -mt-3 mb-3 h-[210px] max-w-screen-2xl md:-mt-5 md:h-[300px]">
         <Image
           priority
           src={HeroImage}
           fill
           className="h-auto w-auto object-cover"
           alt="Pacific Fire Engineering"
+          sizes="(max-width: 600px) 100vw,
+                 (max-width: 1200px) 50vw,
+                                   1200px"
         />
       </div>
     )
