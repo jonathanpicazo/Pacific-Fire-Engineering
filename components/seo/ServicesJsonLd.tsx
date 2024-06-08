e3599ee1d8cc82312f038d3f09fe433f56bb713a
Jonathan Picazo <jonathanpicazo@outlook.com>
2024-06-07 17:54:40 -0700

    feat: fix urls for rich json

--- components/seo/ServicesJsonLd.tsx
+++ components/seo/ServicesJsonLd.tsx
@@ -17,21 +17,21 @@ const ServicesJsonLd: React.FC = () => {
         name: 'Fire Sprinkler Systems',
         description:
           'Pacific Fire Engineering Inc. provides a design service to the developer, architect and owner that allows for the most competitive bidding by your fire sprinkler contractors and insures that the quotations are based on the same design and materials. We will develop a customized set of specifications for your project, detailing the pipe, heads, design criteria, installation requirements, fire pump specifications and code requirements for the contractors to follow.',
-        url: 'https://pacfireeng.com/services/fire-sprinkler-systems',
+        url: 'https://pacfireeng.com/services',
       },
       {
         '@type': 'Offer',
         name: 'High Pile Storage',
         description:
           'Pacific Fire Engineering Inc. will aid the owner, architect, general, or racking contractor in cost-effective, code compliant solutions to all your High Pile storage needs. We work closely with the Authority Having Jurisdiction and negotiate on your behalf so the Building and Fire Codes work for you and not against you.',
-        url: 'https://pacfireeng.com/services/high-pile-storage',
+        url: 'https://pacfireeng.com/services',
       },
       {
         '@type': 'Offer',
         name: 'Chemical Classification - Hazardous Materials Storage',
         description:
           'We will classify your chemicals, classify their commodity, summarize the individual hazard amounts and verify the amounts against the exempt amounts allowed in the Fire and Building codes. Even if you are over the exempt amounts, we can in many cases avoid the need of Hazardous Occupancies (H-Rooms) with the use of Approved Cabinets, Additional Control Areas or negotiating with the approving authority. All that you need to do is supply us with the inventory of hazardous materials (storage and use), the appropriate MSDS Sheets, and a plan showing the areas of your facility that the chemicals are used and stored.',
-        url: 'https://pacfireeng.com/services/chemical-storage',
+        url: 'https://pacfireeng.com/services',
       },
     ],
   };
